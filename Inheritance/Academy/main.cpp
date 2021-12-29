19381a06b4e50e360325b94cb5a1905284869b81
Clayman <Clayman@DESKTOP-DM8E77U>
2021-12-29 20:48:25 +0200

    Polymorphism.

--- Inheritance/Academy/main.cpp
+++ Inheritance/Academy/main.cpp
@@ -3,6 +3,9 @@
 #include<string>
 using namespace std;
 
+#define HUMAN_TAKE_PARAMETERS const std::string& last_name, const std::string& first_name, unsigned int age	//Принимаемые переметры конструктора Human
+#define HUMAN_GIVE_PARAMETERS last_name, first_name, age
+
 class Human
 {
 	std::string last_name;
@@ -35,24 +38,27 @@ public:
 	}
 
 	//					Constructors:
-	Human(const std::string& last_name, const std::string& first_name, unsigned int age)
+	Human(HUMAN_TAKE_PARAMETERS)
 	{
 		set_last_name(last_name);
 		set_first_name(first_name);
 		set_age(age);
 		cout << "HConstructor:\t" << this << endl;
 	}
-	~Human()
+	virtual ~Human()
 	{
 		cout << "HDestructor:\t" << this << endl;
 	}
 
 	//				Methods:
-	void print()const
+	virtual void print()const
 	{
 		cout << last_name << " " << first_name << " " << age << " лет" << endl;
 	}
 };
+
+#define STUDENT_TAKE_PARAMETERS const std::string& speciality, const std::string& group, double rating, double attendance
+#define STUDENT_GIVE_PARAMETERS speciality, group, rating, attendance
 
 class Student :public Human
 {
@@ -95,11 +101,7 @@ public:
 	}
 
 	//					Constructors:
-	Student
-	(
-		const std::string& last_name, const std::string& first_name, unsigned int age,
-		const std::string& speciality, const std::string& group, double rating, double attendance
-	) :Human(last_name, first_name, age)
+	Student(HUMAN_TAKE_PARAMETERS, STUDENT_TAKE_PARAMETERS) :Human(HUMAN_GIVE_PARAMETERS)
 	{
 		set_speciality(speciality);
 		set_group(group);
@@ -118,14 +120,134 @@ public:
 		cout << speciality << " " << group << " " << rating << " " << attendance << endl;
 	}
 };
+
+#define TEACHER_TAKE_PARAMETERS const std::string& speciality, unsigned int experience
+#define TEACHER_GIVE_PARAMETERS speciality, experience
+
+class Teacher :public Human
+{
+	std::string speciality;
+	unsigned int experience;
+public:
+	const std::string& get_speciality()const
+	{
+		return speciality;
+	}
+	unsigned int get_experience()const
+	{
+		return experience;
+	}
+	void set_speciality(const std::string& speciality)
+	{
+		this->speciality = speciality;
+	}
+	void set_experience(unsigned int experience)
+	{
+		this->experience = experience;
+	}
+	//				Constructor
+	Teacher(HUMAN_TAKE_PARAMETERS, TEACHER_TAKE_PARAMETERS) :Human(HUMAN_GIVE_PARAMETERS)
+	{
+		set_speciality(speciality);
+		set_experience(experience);
+		cout << "TConstructor:\t" << this << endl;
+	}
+	~Teacher()
+	{
+		cout << "TDestructor:\t" << this << endl;
+	}
+	//					Methods
+	void print()const
+	{
+		Human::print();
+		cout << speciality << " " << experience << endl;
+	}
+};
+
+class Graduate :public Student
+{
+	std::string subject;
+public:
+	const std::string& get_subject()const
+	{
+		return subject;
+	}
+	void set_subject(const std::string& subject)
+	{
+		this->subject = subject;
+	}
+	//						Constructors:
+	Graduate(HUMAN_TAKE_PARAMETERS, STUDENT_TAKE_PARAMETERS, const std::string& subject)
+		:Student(HUMAN_GIVE_PARAMETERS, STUDENT_GIVE_PARAMETERS)
+	{
+		set_subject(subject);
+		cout << "GConstructor:\t" << this << endl;
+	}
+	~Graduate()
+	{
+		cout << "GDestructor:\t" << this << endl;
+	}
+	//						Methods:
+	void print()const
+	{
+		Student::print();
+		cout << subject << endl;
+	}
+};
+
 //Resharper
+
+//#define INHERITANCE_CHECK
+
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef INHERITANCE_CHECK
 	Human human("Connor", "John", 18);
 	human.print();
 
 	Student stud("Pinkman", "Jessie", 25, "Chemistry", "WW_123", 85, 95);
 	stud.print();
 
+	Teacher teacher("White", "Walter", 50, "Chemistry", 25);
+	teacher.print();
+
+	Graduate grad("Shreder", "Hank", 40, "Cryminalistic", "WW_123", 90, 75, "How to catch Heizenberg");
+	grad.print();
+#endif // INHERITANCE_CHECK
+
+	//Polymorphism (Poly - много, morphis - форма)
+	//Ad-Hoc Polymorphism - перегрузка функций/операторов
+	//Inclusion polymorphism
+	//Upcast - приведение к базовому типу.
+
+	//int arr[] = { 3,5,8,13,21 };
+	//Generalisation (up-cast)
+	Human* group[] =	//Массив людей
+	{
+		new Student("Pinkman", "Jessie", 25, "Chemistry", "WW_123", 85, 95),
+		new Teacher("White", "Walter", 50, "Chemistry", 25),
+		new Graduate("Shreder", "Hank", 40, "Cryminalistic", "WW_123", 90, 75, "How to catch Heizenberg"),
+		new Student("Vercetti", "Tomas", 30, "City business", "Vice", 98, 99),
+		new Teacher("Diaz", "Ricardo", 55, "Weapons distribution", 30),
+		new Student("Montana", "Antonio", 30, "Cryminalistic", "Vice", 90, 80)
+	};
+
+	//Оператор sizeof() возвращает размер в Байтах.
+	cout << sizeof(group)/*/sizeof(Human*)*/ << endl;	//Вычисляем размер массива в элементах
+
+	//Specialisation - уточнение (Down-cast - приведение от базового типа к дочернему)
+	for (int i = 0; i < sizeof(group)/sizeof(Human*); i++)
+	{
+		cout << typeid(*group[i]).name() << endl;//TRRI - Runtime Type Information
+		//http://cplusplus.com/doc/tutorial/typecasting/
+		group[i]->print();
+		cout << "\n-----------------------------------------------\n";
+	}
+
+	////////////////////////////////////////
+	for (int i = 0; i < sizeof(group) / sizeof(group[0]); i++)
+	{
+		delete group[i];
+	}
 }

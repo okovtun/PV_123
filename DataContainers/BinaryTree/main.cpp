d9678eadb1f0db3118928458f07b1b494fd8743e
Clayman <Clayman@DESKTOP-DM8E77U>
2022-03-02 20:36:07 +0200

    TODO: Erase.

--- DataContainers/BinaryTree/main.cpp
+++ DataContainers/BinaryTree/main.cpp
@@ -4,13 +4,14 @@ using namespace std;
 
 class Tree
 {
+protected:
 	class Element
 	{
 		int Data;
 		Element* pLeft;
 		Element* pRight;
 	public:
-		Element(int Data, Element* pLeft=nullptr, Element* pRight=nullptr) :Data(Data), pLeft(pLeft), pRight(pRight)
+		Element(int Data, Element* pLeft = nullptr, Element* pRight = nullptr) :Data(Data), pLeft(pLeft), pRight(pRight)
 		{
 			cout << "EConstructor:\t" << this << endl;
 		}
@@ -19,6 +20,7 @@ class Tree
 			cout << "EDestructor:\t" << this << endl;
 		}
 		friend class Tree;
+		friend class UniqueTree;
 	}*Root;
 public:
 	Element* getRoot()const
@@ -30,14 +32,32 @@ public:
 		this->Root = nullptr;
 		cout << "TConstructor:\t" << this << endl;
 	}
+	Tree(const std::initializer_list<int>& il) :Tree()
+	{
+		for (int const* it = il.begin(); it != il.end(); it++)
+		{
+			insert(*it, Root);
+		}
+	}
+	Tree(const Tree& other):Tree()
+	{
+		copy(other.Root);
+		cout << "CopyConstructor:\t" << this << endl;
+	}
 	~Tree()
 	{
+		clear();
 		cout << "TDestructor:\t" << this << endl;
 	}
 
 	void insert(int Data)
 	{
 		insert(Data, Root);
+	}
+	void clear()
+	{
+		clear(Root);
+		Root = nullptr;
 	}
 	int minValue()const
 	{
@@ -51,9 +71,18 @@ public:
 	{
 		return size(Root);
 	}
+	int sum()const
+	{
+		return sum(Root);
+	}
+	double avg()const
+	{
+		return (double)sum(Root) / size(Root);
+	}
 	void print()const
 	{
 		print(Root);
+		cout << endl;
 	}
 private:
 	void insert(int Data, Element* Root)
@@ -75,6 +104,20 @@ private:
 			else insert(Data, Root->pRight);
 		}
 	}
+	void clear(Element* Root)
+	{
+		if (Root == nullptr)return;
+		clear(Root->pLeft);
+		clear(Root->pRight);
+		delete Root;
+	}
+	void copy(Element* Root)
+	{
+		if (Root == nullptr)return;
+		insert(Root->Data, this->Root);
+		copy(Root->pLeft);
+		copy(Root->pRight);
+	}
 
 	int minValue(Element* Root)const
 	{
@@ -93,6 +136,10 @@ private:
 	{
 		return Root == nullptr ? 0 : size(Root->pLeft) + size(Root->pRight) + 1;
 	}
+	int sum(Element* Root)const
+	{
+		return !Root ? 0 : sum(Root->pLeft) + sum(Root->pRight) + Root->Data;
+	}
 
 	void print(Element* Root)const
 	{
@@ -103,19 +150,61 @@ private:
 	}
 };
 
+class UniqueTree :public Tree
+{
+	void insert(int Data, Element* Root)
+	{
+		if (this->Root == nullptr)
+		{
+			this->Root = new Element(Data);
+			return;
+		}
+
+		if (Root == nullptr)return;
+		if (Data < Root->Data)
+		{
+			if (Root->pLeft == nullptr)Root->pLeft = new Element(Data);
+			else insert(Data, Root->pLeft);
+		}
+		else if (Data > Root->Data)
+		{
+			if (Root->pRight == nullptr)Root->pRight = new Element(Data);
+			else insert(Data, Root->pRight);
+		}
+	}
+public:
+	void insert(int Data)
+	{
+		insert(Data, this->Root);
+	}
+};
+
+//#define BASE_CHECK
+
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef BASE_CHECK
+	cout << typeid(nullptr).name() << endl;
 	int n;
 	cout << "Введите размер дерева: "; cin >> n;
-	Tree tree;
+	UniqueTree tree;
 	for (int i = 0; i < n; i++)
 	{
 		tree.insert(rand() % 100);
 	}
+	//tree.clear();
 	tree.print();
 	cout << endl;
 	cout << "Минимальное значение в дереве: " << tree.minValue() << endl;
 	cout << "Максимальное значение в дереве: " << tree.maxValue() << endl;
 	cout << "Количество элементов дерева: " << tree.size() << endl;
+	cout << "Сумма элементов дерева:\t\t" << tree.sum() << endl;
+	cout << "Среднее арифметическое элементов дерева: " << tree.avg() << endl;
+#endif // BASE_CHECK
+
+	Tree tree = { 50, 25, 75, 16, 32, 64, 80 };
+	tree.print();
+	Tree tree2 = tree;
+	tree2.print();
 }
--- DataContainers/HomeWork/main.cpp
+++ DataContainers/HomeWork/main.cpp
@@ -1,457 +1,236 @@
-﻿#include <iostream>
+﻿#include<iostream>
+using namespace std;
 
-#define tab "\t"
-// #define DEBUG
-
-template <class T>
-class ForwardList;
-
-template <class T>
-class Element
+class Tree
 {
-	T data_;
-	Element *pNext;
-
-public:
-	Element(T data = T{}, Element *pNext = nullptr) : data_(data), pNext(pNext)
+protected:
+	class Element
 	{
-#ifdef DEBUG
-		std::cout << "EConstrcutor:\t" << this << std::endl;
-#endif
-	}
-	~Element()
-	{
-#ifdef DEBUG
-		std::cout << "EDestrcutor:\t" << this << std::endl;
-#endif
-	}
-
-	Element const *const &next() const
-	{
-		return pNext;
-	}
-
-	Element *&next()
-	{
-		return pNext;
-	}
-
-	T const &data() const
-	{
-		return data_;
-	}
-
-	T &data()
-	{
-		return data_;
-	}
-
-	friend class ForwardList<T>;
-};
-
-template <class T>
-class ForwardList
-{
-	using _node = Element<T>;
-
-	_node *head_;
-	size_t size_;
-
-	template <class _FLNodeType, class _FLNodePtr, class _FLNodeRef>
-	class iterator_impl
-	{
-		_FLNodePtr ptr_;
-
+		int Data;
+		Element* pLeft;
+		Element* pRight;
 	public:
-		using _Self = ForwardList::iterator_impl<_FLNodeType, _FLNodePtr, _FLNodeRef>;
-
-		using difference_type = std::ptrdiff_t; // auto size pointer
-		using iterator_category = std::forward_iterator_tag;
-
-		iterator_impl() : ptr_(nullptr) {}
-		iterator_impl(_FLNodePtr p) : ptr_(p) {}
-		iterator_impl(const _Self &) = default;
-		iterator_impl(_Self &&) = default;
-		_Self &operator=(_Self &&) = default;
-		_Self &operator=(const _Self &) = default;
-
-		_Self &operator++()
+		Element(int Data, Element* pLeft = nullptr, Element* pRight = nullptr) :Data(Data), pLeft(pLeft), pRight(pRight)
 		{
-			ptr_ = ptr_->next();
-			return *this;
+			//cout << "EConstructor:\t" << this << endl;
 		}
-
-		_Self operator++(int)
+		~Element()
 		{
-			_Self temp(*this);
-			ptr_ = ptr_->next();
-			return temp;
+			//cout << "EDestructor:\t" << this << endl;
 		}
-
-		decltype(ptr_->data()) operator*()
-		{
-			return ptr_->data();
-		}
-
-		decltype(&(ptr_->data())) operator->()
-		{
-			return &(ptr_->data());
-		}
-
-		bool operator!=(const _Self &it) const
-		{
-			return ptr_ != it.ptr_;
-		}
-
-		bool operator==(const _Self &it) const
-		{
-			return ptr_ == it.ptr_;
-		}
-	};
-
-	template <class It>
-	_node *advance(_node *iter, size_t distance)
-	{
-		if (distance > size_)
-			throw "Forward list out of bounds";
-
-		for (size_t i = 0; i < distance; ++i)
-		{
-			iter = iter->pNext;
-		}
-		return iter;
-	}
-
+		friend class Tree;
+		friend class UniqueTree;
+	}*Root;
 public:
-	using iterator = ForwardList::iterator_impl<_node, _node *, _node &>;
-	using const_iterator = ForwardList::iterator_impl<const _node, const _node *, const _node &>;
-
-	ForwardList() noexcept : head_(nullptr), size_(0)
+	Element* getRoot()const
 	{
-#ifdef DEBUG
-		std::cout << "LConstructor:\t" << this << std::endl;
-#endif
+		return Root;
 	}
-
-	ForwardList(size_t size) : ForwardList()
+	Tree()
 	{
-		while (size_ != size)
+		this->Root = nullptr;
+		//cout << "TConstructor:\t" << this << endl;
+	}
+	Tree(const std::initializer_list<int>& il) : Tree()
+	{
+		for (int const* it = il.begin(); it != il.end(); it++)
 		{
-			this->push_front(T());
+			insert(*it, Root);
 		}
 	}
-
-	ForwardList(std::initializer_list<T> il) : ForwardList()
+	Tree(const Tree& other):Tree()
 	{
-		for (T val : il)
+		copy(other.Root);
+		//cout << "CopyConstructor: \t" << this << endl;
+	}
+	~Tree()
+	{
+		clear();
+		//cout << "TDestructor:\t" << this << endl;
+	}
+
+	void insert(int Data)
+	{
+		insert(Data, Root);
+	}
+	void clear()
+	{
+		clear(Root);
+		Root = nullptr;
+	}
+	int minValue()const
+	{
+		return minValue(Root);
+	}
+	int maxValue()const
+	{
+		return maxValue(Root);
+	}
+	void print()
+	{
+		print(Root);
+		cout << endl;
+	}
+	int size()const
+	{
+		return size(Root);
+	}
+	int sum()const
+	{
+		return sum(Root);
+	}
+
+	double avg()
+	{
+		return (double)sum(Root) / size(Root);
+	}
+
+private:
+	void insert(int Data, Element* Root)
+	{
+		if (this->Root == nullptr)
 		{
-			this->push_front(val);
-		}
-	}
-
-	~ForwardList()
-	{
-		while (head_)
-		{
-			this->pop_front();
-		}
-#ifdef DEBUG
-		std::cout << "LDestructor:\t" << this << std::endl;
-#endif
-	}
-
-	ForwardList(ForwardList<T> const &other) : ForwardList()
-	{
-		for (T i : other)
-			this->push_front(i);
-		this->reverse();
-	}
-
-	ForwardList(ForwardList<T> &&other)
-	{
-		_node *tmp = this->head_;
-
-		this->head_ = other.head_;
-		this->size_ = other.size_;
-
-		other.head_ = tmp;
-	}
-
-	ForwardList<T> &operator=(const ForwardList<T> &other)
-	{
-		if (this == &other)
-			return *this;
-		ForwardList<T> to_delete = std::move(*this);
-		new (this) ForwardList(other);
-		return *this;
-	}
-
-	ForwardList<T> &operator=(ForwardList<T> &&other)
-	{
-		if (this == &other)
-			return *this;
-
-		_node *tmp = this->head_;
-
-		this->head_ = other.head_;
-		this->size_ = other.size_;
-
-		other.head_ = tmp;
-		return *this;
-	}
-
-	inline void push_front(T data)
-	{
-		head_ = new Element<T>(data, head_);
-
-		++this->size_;
-	}
-
-	void push_back(T data)
-	{
-		if (head_ == nullptr)
-			return this->push_front(data);
-		_node *iter = ForwardList::advance(head_, size_ - 1);
-		iter->pNext = new Element(data);
-		++this->size_;
-	}
-
-	inline void pop_front()
-	{
-		if (head_ == nullptr)
+			this->Root = new Element(Data);
 			return;
+		}
 
-		_node *temp = head_;
-		head_ = head_->pNext;
-		delete temp;
-
-		--this->size_;
+		if (Root == nullptr)return;
+		if (Data < Root->Data)
+		{
+			if (Root->pLeft == nullptr)Root->pLeft = new Element(Data);
+			else insert(Data, Root->pLeft);
+		}
+		else
+		{
+			if (Root->pRight == nullptr)Root->pRight = new Element(Data);
+			else insert(Data, Root->pRight);
+		}
 	}
 
-	void pop_back()
+	void clear(Element* Root)
 	{
-		if (head_ == nullptr)
+		if (Root == nullptr)
 			return;
-		if (head_->pNext == nullptr)
-			return this->pop_front();
-		_node *iter = ForwardList::advance(head_, size_ - 2);
-		delete iter->pNext;
-		iter->pNext = nullptr;
-
-		--this->size_;
+		clear(Root->pLeft); // удаляется вся левая ветка с минимального значения
+		clear(Root->pRight); // удаляется вся правая ветка с максимального значения
+		delete Root;
+	}
+	void copy(Element* Root)
+	{
+		if (Root == nullptr)
+			return;
+		insert(Root->Data, this->Root);
+		copy(Root->pLeft);
+		copy(Root->pRight);
 	}
 
-	void insert(size_t index, T data)
+	int minValue(Element* Root)const
 	{
-		if (index == 0 or head_ == nullptr)
-			return this->push_front(data);
-		_node *iter = ForwardList::advance(head_, index - 1);
-		_node *temp = iter->pNext;
-		iter->pNext = new Element(data, temp);
-		++this->size_;
+		if (Root == nullptr)
+			return 0;
+		//        else if (Root -> pLeft == nullptr)
+		//            return Root -> Data;
+		//        else return minValue(Root -> pLeft);
+
+				//return Root -> pLeft == nullptr ? Root -> Data : minValue(Root -> pLeft);
+		return Root->pLeft ? minValue(Root->pLeft) : Root->Data;
 	}
 
-	void erase(size_t index)
+	int maxValue(Element* Root)const
 	{
-		if (index == 0)
-			return this->pop_front();
-		_node *iter = ForwardList::advance(head_, index - 1);
-		if (iter->pNext == nullptr)
-			throw "No element at given index";
-		_node *temp = iter->pNext->pNext;
-		delete iter->pNext;
-		iter->pNext = temp;
-		--this->size_;
+		if (Root == nullptr)
+			return 0;
+		//        else if (Root -> pRight == nullptr)
+		//            return Root -> Data;
+		//        else return maxValue(Root -> pRight);
+
+		return Root->pRight == nullptr ? Root->Data : maxValue(Root->pRight);
+	}
+	int size(Element* Root)const
+	{
+		if (Root == nullptr)
+			return 0;
+		return size(Root->pLeft) + size(Root->pRight) + 1;
 	}
 
-	void unique() //как быстрее?
+	int sum(Element* Root)const
 	{
-		Element<T> *to_unique = this->head_;
-		Element<T> *to_find = nullptr;
-
-		for (size_t unique_idx = 0; to_unique; ++unique_idx, to_unique = to_unique->pNext)
+		if (Root == nullptr)
 		{
-			to_find = to_unique->pNext;
-
-			for (size_t idx_to_remove = unique_idx + 1; to_find; ++idx_to_remove)
-			{
-				if (to_unique->data_ == to_find->data_)
-				{
-					to_find = to_find->pNext;
-					this->erase(idx_to_remove);
-					--idx_to_remove;
-				}
-				else
-				{
-					to_find = to_find->pNext;
-				}
-			}
+			return 0;
 		}
+		return sum(Root->pLeft) + sum(Root->pRight) + Root->Data;
+		//return !Root ? 0 : sum(Root->pLeft) + sum(Root->pRight) + Root -> Data;
 	}
 
-	void reverse()
+	void print(Element* Root)const
 	{
-		_node *prev = nullptr;
-		_node *next = this->head_;
-		_node *temp = nullptr;
-		for (size_t idx = 0; idx < size_; ++idx)
-		{
-			temp = next->pNext;
-			next->pNext = prev;
-			prev = next;
-			next = temp;
-		}
-		this->head_ = prev;
-	}
-
-	T operator[](size_t index) const
-	{
-		return ForwardList::advance(head_, index)->data_;
-	}
-
-	T &operator[](size_t index)
-	{
-		return ForwardList::advance(head_, index)->data_;
-	}
-
-	bool is_empty() const { return !head_; }
-
-	void print() const
-	{
-		for (auto Temp = head_; Temp; Temp = Temp->pNext)
-		{
-			std::cout << Temp << tab << Temp->data_ << tab << Temp->pNext << std::endl;
-		}
-	}
-
-	ForwardList::const_iterator begin() const
-	{
-		return ForwardList::const_iterator(head_);
-	}
-
-	ForwardList::const_iterator end() const
-	{
-		return ForwardList::const_iterator();
-	}
-
-	ForwardList::iterator begin()
-	{
-		return ForwardList::iterator(head_);
-	}
-
-	ForwardList::iterator end()
-	{
-		return ForwardList::iterator();
+		if (Root == nullptr) return;
+		print(Root->pLeft);
+		cout << Root->Data << "\t";
+		print(Root->pRight);
 	}
 };
 
-// #define BASE_CHECK
-// #define DESTRUCTOR_CHECK
-// #define HOME_WORK_1
-// #define HOME_WORK_2
-#define RANGE_BASE_FOR_LIST
-
-ForwardList<int> func()
+class UniqueTree : public Tree
 {
-	return ForwardList<int>({ 4, 3, 2, 1 });
-}
+	void insert(int Data, Element* Root)
+	{
+		if (this->Root == nullptr)
+		{
+			this->Root = new Element(Data);
+			return;
+		}
+
+		if (Root == nullptr)return;
+		if (Data < Root->Data)
+		{
+			if (Root->pLeft == nullptr)Root->pLeft = new Element(Data);
+			else insert(Data, Root->pLeft);
+		}
+		else if (Data > Root->Data)
+		{
+			if (Root->pRight == nullptr)Root->pRight = new Element(Data);
+			else insert(Data, Root->pRight);
+		}
+	}
+
+public:
+	void insert(int Data)
+	{
+		insert(Data, this->Root);
+	}
+};
+
+//#define BASE_CHECK
 
 int main()
 {
 #ifdef BASE_CHECK
 	int n;
-	std::cout << "Enter list size: ";
-	std::cin >> n;
-	ForwardList<int> list;
-	list.pop_front();
+	cout << "Enter size of the tree: "; cin >> n;
+
+	UniqueTree tree;
 	for (int i = 0; i < n; i++)
 	{
-		// list.push_front(rand() % 100);
-		list.push_back(rand() % 100);
+		tree.insert(rand() % 100);
 	}
-	list.print();
-	// list.push_back(123);
-	// list.pop_front();
-	// list.pop_back();
+	tree.print();
+	cout << endl;
 
-	int index;
-	int value;
-	std::cout << "Enter index INSERT element: ";
-	std::cin >> index;
-	std::cout << "Enter value INSERT element: ";
-	std::cin >> value;
-
-	list.insert(index, value);
-	list.print();
-
-	std::cout << "Enter index ERASE element: ";
-	std::cin >> index;
-	list.erase(index);
-	list.print();
-
+	cout << "Min value in the tree: " << tree.minValue() << endl;
+	cout << "Max value in the tree: " << tree.maxValue() << endl;
+	cout << "Size of the tree: " << tree.size() << endl;
+	cout << "Sum of elements: " << tree.sum() << endl;
+	cout << "Average: " << tree.avg() << endl;
 #endif // BASE_CHECK
 
-	// ForwardList<int> list1;
-	// list1.push_back(3);
-	// list1.push_back(5);
-	// list1.push_back(8);
-	// list1.push_back(13);
-	// list1.push_back(21);
-	// list1.pop_back();
-	// list1.pop_front();
-	// list1.print();
+	Tree tree = { 50, 25, 75, 16, 32, 64, 80 };
+	tree.print();
 
-#ifdef DESTRUCTOR_CHECK
-	int n;
-	std::cout << "Enter list size: ";
-	std::cin >> n;
-	ForwardList<int> list;
-	for (int i = 0; i < n; i++)
-	{
-		list.push_front(rand() % 100);
-	}
-	// std::cout << "Список заполнен" << std::endl;
-	list.print();
-#endif // DESTRUCTOR_CHECK
+	Tree tree2 = tree;
+	tree2.print();
 
-#ifdef HOME_WORK_1
-	int n;
-	std::cout << "Enter list size: ";
-	std::cin >> n;
-	ForwardList<int> list(n);
-	for (int i = 0; i < n; i++)
-	{
-		list[i] = rand() % 100;
-	}
-	for (int i = 0; i < n; i++)
-	{
-		std::cout << list[i] << tab;
-	}
-	std::cout << std::endl;
-#endif // HOME_WORK_1
 
-#ifdef HOME_WORK_2
-	ForwardList<int> list = { 3, 5, 8, 13, 3, 21, 5, 5, 6, 5, 13 };
-	list.unique();
-	list.reverse();
-	list.print();
-#endif // HOME_WORK_2
-
-#ifdef RANGE_BASE_FOR_LIST
-	ForwardList<int> list = { 3, 5, 8, 13, 21 }; // https://docs.microsoft.com/en-us/cpp/cpp/range-based-for-statement-cpp?view=msvc-160
-	for (int i : list)
-	{
-		std::cout << i << tab;
-	}
-	std::cout << std::endl;
-
-	ForwardList<int> l2{};
-	l2 = func();
-	l2 = list;
-	for (int i : l2)
-	{
-		std::cout << i << tab;
-	}
-	std::cout << std::endl;
-#endif // RANGE_BASE_FOR_LIST
+	return 0;
 }

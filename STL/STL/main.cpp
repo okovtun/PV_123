affd2715ffa9db11e0406c56f52b3941013d311c
Clayman <Clayman@DESKTOP-DM8E77U>
2022-03-06 20:39:00 +0200

    STL start.

---
+++ STL/STL/main.cpp
@@ -0,0 +1,55 @@
+﻿//STL - Standard Template Library
+#include<iostream>
+#include<array>
+#include<vector>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+
+//#define STL_ARRAY
+#define STL_VECTOR
+
+void main()
+{
+	setlocale(LC_ALL, "");
+#ifdef STL_ARRAY
+	//array - это контейнер, который хранит данные в виде статического массива.
+	std::array<int, 5> arr = { 3,5,8,13,21 };
+	for (int i = 0; i < arr.size(); i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+
+	std::array<int, 5>* p_arr = new std::array<int, 5>{3, 5, 8, 13, 21};
+	for (int i = 0; i < p_arr->size(); i++)
+	{
+		cout << p_arr->operator[](i) << tab;
+	}
+	cout << endl;
+#endif //STL_ARRAY
+
+#ifdef STL_VECTOR
+	//vector - это контейнер, который хранит данные в виде динамического массива.
+	std::vector<int> vec = { 0,1,1,2,3,5,8,13,21,34 };
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec[i] << tab;
+	}
+	cout << endl;
+	cout << "Size:    " << vec.size() << endl;
+	cout << "MaxSize: " << vec.max_size() << endl;
+	cout << "Capacity:" << vec.capacity() << endl;
+	vec.push_back(55);
+	vec.pop_back();
+	vec.pop_back();
+	vec.pop_back();
+	cout << "Size:    " << vec.size() << endl;
+	cout << "MaxSize: " << vec.max_size() << endl;
+	cout << "Capacity:" << vec.capacity() << endl;
+
+#endif // STL_VECTOR
+
+}

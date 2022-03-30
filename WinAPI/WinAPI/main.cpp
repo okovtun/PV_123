b2f6b1b6438564aa04a57ea2e48090af755293ab
Clayman <Clayman@DESKTOP-DM8E77U>
2022-03-30 21:50:45 +0300

    DialogBoxes

--- WinAPI/WinAPI/main.cpp
+++ WinAPI/WinAPI/main.cpp
@@ -1,18 +1,21 @@
-#include<Windows.h>
+﻿#include<Windows.h>
 #include"resource.h"
+
+CHAR sz_login_invitation[] = "Введеите логин";
+CHAR sz_password_invitation[] = "Введеите пароль";
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMgs, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-	/*MessageBox(NULL, "HelloWorld", "Info", MB_YESNOCANCEL | MB_ICONERROR | MB_HELP 
+	/*MessageBox(NULL, "HelloWorld", "Info", MB_YESNOCANCEL | MB_ICONERROR | MB_HELP
 		| MB_DEFBUTTON4 | MB_SYSTEMMODAL
 		//| MB_RTLREADING
 		|MB_SETFOREGROUND);*/
-	//https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-messagebox
+		//https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-messagebox
 
 	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc, 0);
-	
+
 	return 0;
 }
 
@@ -20,8 +23,57 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
-	case WM_INITDIALOG:break;
-	case WM_COMMAND:break;
+	case WM_INITDIALOG:
+	{
+		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+
+		SendMessage(GetDlgItem(hwnd, IDC_EDIT_LOGIN), WM_SETTEXT, 0, (LPARAM)sz_login_invitation);
+		SendMessage(GetDlgItem(hwnd, IDC_EDIT_PASSWORD), WM_SETTEXT, 0, (LPARAM)sz_password_invitation);
+	}
+	break;
+	case WM_COMMAND:
+	{
+		switch (LOWORD(wParam))
+		{
+		case IDC_EDIT_LOGIN:
+		{
+			//При выполнении различных действий над каким-либо элементов интерфейса (окном),
+			//этот элемент интерфейса (окно) получает уведомления. Уведосления, это самые обычные сообщения.
+			//Например, когда мы становимся в текстовое поле, то это текстовое поле получает уведомление EN_SETFOCUS,
+			//когда мы "уходим" из текстового поля, то оно получает уведомление EN_KILLFOCUS.
+			//EN - Edit Notification
+			//Фокус - это часть окна, которая принимает команды с клавиатуры.
+			switch (HIWORD(wParam))
+			{
+			case EN_SETFOCUS:SendMessage(GetDlgItem(hwnd, IDC_EDIT_LOGIN), WM_SETTEXT, 0, (LPARAM)""); break;
+			}
+		}
+		break;
+		case IDC_BUTTON_COPY:
+		{
+			CONST INT SIZE = 256;	//Размер буфера
+			CHAR sz_buffer[SIZE] = {};
+			HWND hLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);	//Получаем ID окна текстового поля по имени ресурса
+			SendMessage(hLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);//Копируем текст из текстового поля Password в sz_buffer
+			HWND hPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+			SendMessage(hPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+		break;
+		case IDC_BUTTON_SET_TITLE:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
+			SendMessage(GetDlgItem(hwnd, IDC_EDIT_LOGIN), WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			//SendMessage(FindWindow(NULL, "WinAPI - Microsoft Visual Studio"), WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+		break;
+		case IDOK:MessageBox(NULL, "Была нажата кнопка OK", "Info", MB_OK | MB_ICONWARNING); break;
+		case IDCANCEL: EndDialog(hwnd, 0); break;
+		}
+	}
+	break;
 	case WM_CLOSE:
 		EndDialog(hwnd, 0);
 	}
--- WinAPI/WinAPI/resource.h
+++ WinAPI/WinAPI/resource.h
@@ -3,14 +3,22 @@
 // Used by Resource.rc
 //
 #define IDD_DIALOG1                     101
+#define IDI_ICON1                       103
+#define IDC_STATIC_LOGIN                1001
+#define IDC_EDIT_LOGIN                  1002
+#define IDC_STATIC_PASSWORD             1003
+#define IDC_EDIT_PASSWORD               1004
+#define IDC_BUTTON_COPY                 1005
+#define IDC_BUTTON2                     1006
+#define IDC_BUTTON_SET_TITLE            1006
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif

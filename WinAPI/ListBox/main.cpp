02293d5fdc121d0dbf3d1d118ff0f049fa6f1899
Oleg <31523835+okovtun@users.noreply.github.com>
2022-04-03 21:26:49 +0300

    Book.

---
+++ WinAPI/ListBox/main.cpp
@@ -0,0 +1,35 @@
+﻿//ListBox
+#include<Windows.h>
+#include"resource.h"
+
+//type name(parameters);
+//type calling_convention name(parameters);
+//type - тип возвращаемого значение: bool, char, int......
+//calling_convention - конвенция вызовов (соглашение о вызове функции):
+//__syscall
+//__stdcall	 - WinAPI
+//__fastcall - x64
+//__cdecl	 - C/C++
+//Конвенция вызовов определяет порядок вызова функции:
+//	- кто очищает стек, вызывающая функция (caller) или вызываемая функция (callee)
+//	- как читается список передаваемых параметров - слева направо, или справа налево,
+//	  или же параметры передаются через регистры (__fastcall)
+//https://docs.microsoft.com/en-us/cpp/cpp/argument-passing-and-naming-conventions?view=msvc-170
+//https://docs.microsoft.com/en-us/cpp/cpp/cdecl?view=msvc-170
+//https://docs.microsoft.com/ru-ru/cpp/cpp/cdecl?view=msvc-170
+//http://cplusplus.com/reference/cstdarg/
+
+//-----------------------------------------------------------------------------------------------
+
+//HINSTANCE hInstance - экземпляр исполняемого файла (*.exe). Это загруженный в память *.exe-файл нашей программы.
+//HINSTANCE рPrevInst - предыдущий экзепляр *.exe-файла. Этот параметр использовался в Windows 98, и оставлен только для совместимости.
+//LPSTR lpCmdLine - командная строка, с которой была запущена программа. 
+//LPSTR: LP - LongPointer STR - String => LPSTR - LongPointer to String (Указатель на строку - char*)
+//int cCmdShow - режим отображения окна: свернуто в окно, развернуто на весь экран, свернуто на панель задач....
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
+{
+	//HWND - Handler to Window. ОБРАБОТЧИК ОКНА, дескриптор окна.
+	//HWND - это идентификатор окна, через который к нему можно обратиться.
+	//HWND - это как имя переменной, или функции.
+	return 0;
+}
--- WinAPI/WinAPI/main.cpp
+++ WinAPI/WinAPI/main.cpp
@@ -34,6 +34,10 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	break;
 	case WM_COMMAND:
 	{
+		CONST INT SIZE = 256;
+		CHAR sz_buffer[SIZE]{};
+		HWND hLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);	//Получаем ID окна текстового поля по имени ресурса
+		HWND hPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
 		switch (LOWORD(wParam))
 		{
 		case IDC_EDIT_LOGIN:
@@ -44,16 +48,32 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			//когда мы "уходим" из текстового поля, то оно получает уведомление EN_KILLFOCUS.
 			//EN - Edit Notification
 			//Фокус - это часть окна, которая принимает команды с клавиатуры.
+			/*CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};*/
+			SendMessage(hLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
 			switch (HIWORD(wParam))
 			{
-			case EN_SETFOCUS:SendMessage(GetDlgItem(hwnd, IDC_EDIT_LOGIN), WM_SETTEXT, 0, (LPARAM)""); break;
+			case EN_SETFOCUS:
+				if (strcmp(sz_buffer, sz_login_invitation) == 0)
+					SendMessage(hLogin, WM_SETTEXT, 0, (LPARAM)""); 
+				break;
+			case EN_KILLFOCUS:
+				if (strlen(sz_buffer) == 0)
+					SendMessage(hLogin, WM_SETTEXT, 0, (LPARAM)sz_login_invitation);
+				break;
 			}
+			/*for (int i = 0; i < strlen(sz_buffer); i++)
+				sz_buffer[i] += 32;
+			int n = strlen(sz_buffer);
+			for (int i = 0; i < n; i++)
+				sz_buffer[i] += 32;*/
+
 		}
 		break;
 		case IDC_BUTTON_COPY:
 		{
-			CONST INT SIZE = 256;	//Размер буфера
-			CHAR sz_buffer[SIZE] = {};
+			//CONST INT SIZE = 256;	//Размер буфера
+			//CHAR sz_buffer[SIZE] = {};
 			HWND hLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);	//Получаем ID окна текстового поля по имени ресурса
 			SendMessage(hLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);//Копируем текст из текстового поля Password в sz_buffer
 			HWND hPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
@@ -62,8 +82,8 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		break;
 		case IDC_BUTTON_SET_TITLE:
 		{
-			CONST INT SIZE = 256;
-			CHAR sz_buffer[SIZE] = {};
+			//CONST INT SIZE = 256;
+			//CHAR sz_buffer[SIZE] = {};
 			SendMessage(GetDlgItem(hwnd, IDC_EDIT_LOGIN), WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
 			SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer);
 			//SendMessage(FindWindow(NULL, "WinAPI - Microsoft Visual Studio"), WM_SETTEXT, 0, (LPARAM)sz_buffer);

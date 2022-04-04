e374ee3809f27a9df754a55a4ce06861a5fc5ef7
Oleg <31523835+okovtun@users.noreply.github.com>
2022-04-04 21:26:45 +0300

    ListBox DONE.

--- WinAPI/ListBox/main.cpp
+++ WinAPI/ListBox/main.cpp
@@ -1,6 +1,19 @@
 ﻿//ListBox
+#define _CRT_SECURE_NO_WARNINGS	//для sprintf()
 #include<Windows.h>
+#include<cstdio>	//для sprintf()
 #include"resource.h"
+
+CONST CHAR* string[] =
+{
+	//Вот эти строки мы добавим в ListBox:
+	"Первая строка",
+	"Вторая строка",
+	"Третья строка",
+	"This", "is", "my", "first", "List", "Box"
+};
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 //type name(parameters);
 //type calling_convention name(parameters);
@@ -28,8 +41,61 @@
 //int cCmdShow - режим отображения окна: свернуто в окно, развернуто на весь экран, свернуто на панель задач....
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
 {
-	//HWND - Handler to Window. ОБРАБОТЧИК ОКНА, дескриптор окна.
-	//HWND - это идентификатор окна, через который к нему можно обратиться.
-	//HWND - это как имя переменной, или функции.
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, DlgProc, 0);
 	return 0;
+}
+
+
+//HWND (Handler to Window) - Handler to Window. ОБРАБОТЧИК ОКНА, дескриптор окна.
+//HWND - это идентификатор окна, через который к нему можно обратиться.
+//HWND - это как имя переменной, или функции.
+//HWND - ЭТО ОКНО!
+//UINT uMsg - Message (сообщение). UINT - unsigned int.
+//wParam, lParam - это параметры сообщения, они зависят от того, какое именно uMsg мы посылаем окну
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:	//Это сообщение отправляется в окно при его создании
+	{
+		HWND hListBox = GetDlgItem(hwnd, IDC_LIST1);
+		//GetDlgItem(hwnd, IDC_LIST1); возвращает HWND элемента окна по ID-ресурса нужного элемента
+		for (int i = 0; i < sizeof(string) / sizeof(string[0]); i++)
+		{
+			SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)string[i]);
+			//Сообщение LB_ADDSTRIG добавляет указанную строку в ListBox.
+		}
+	}
+	break;
+	case WM_COMMAND:	//Это сообщение обрабатывает нажатие на кнопки, и другие элементы интерфейса окна
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};	//в эту строку мы скопируем выделенную строку ListBox.
+			CHAR sz_message[SIZE] = {};	//этy строку мы будем отображать в MessageBox.
+
+			HWND hListBox = GetDlgItem(hwnd, IDC_LIST1);
+			int index = SendMessage(hListBox, LB_GETCURSEL, 0, 0);	//Получаем числовой индекс выделенного элемента ListBox
+			SendMessage(hListBox, LB_GETTEXT, index, (LPARAM)sz_buffer);
+			sprintf(sz_message, "Вы выбрали элемен № %d со значением \"%s\".", index, sz_buffer);
+			//Функция sprintf() выполняет форматирование строк
+			//sz_message - это буфер, в который сохраниться отформатированная строка
+			//"Вы выбрали... - это шаблон строки, т.е. то, как она должна выглядеть
+			//%d - вставить в шаблонную строку десятичное целое число (decimal)
+			//%s - вставить в шаблонную строку подстроку.
+			//Все вставляемые значения перечислены далее в передаваемых параметрах: index, sz_buffer....
+			//Количество параметров функции sprintf() неограничено.
+			//Функция sprintf() позволяет вставлять в строку какие угодно значения.
+			MessageBox(hwnd, sz_message, "Info", MB_OK | MB_ICONINFORMATION | MB_HELP);
+			//
+		}
+			break;
+		case IDCANCEL:EndDialog(hwnd, 0); break;
+		}
+		break;
+	case WM_CLOSE: EndDialog(hwnd, 0);
+	}
+	return FALSE;
 }
